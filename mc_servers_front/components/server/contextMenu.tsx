5ae24b31e3a80025a6fe001d696796d5548fead7
0omanuo0 <manu365manu@gmail.com>
2024-04-15 18:30:00 +0000

    Update context menu

--- mc_servers_front/components/server/contextMenu.tsx
+++ mc_servers_front/components/server/contextMenu.tsx
@@ -1,6 +1,19 @@
 import React, { useEffect } from "react";
 
 
+enum ContextMenuOptions {
+    DELETE   = "delete",
+    RENAME   = "rename",
+    FILE     = "file",
+    FOLDER   = "folder",
+    UPLOAD   = "upload",
+    DOWNLOAD = "download"
+}
+
+const isFile = (path: string) => {
+    return path.split(".").pop() === path ? undefined : path.split('.').pop();
+}
+
 
 export default function ContextMenu( {file, position}: {file: string; position: [number, number]}) {
     // 
@@ -9,6 +22,9 @@ export default function ContextMenu( {file, position}: {file: string; position: 
         // prevent parect onclick
         e.preventDefault();
         console.log(e.currentTarget.id + file);
+        if(e.currentTarget.id === ContextMenuOptions.DOWNLOAD){
+            console.log("Option: Download");
+        }
     }
     
     // move menu to mouse position when useEffect is called
@@ -18,6 +34,8 @@ export default function ContextMenu( {file, position}: {file: string; position: 
             menu.style.left = `${position[0]}px`;
             menu.style.top = `${position[1]}px`;
         }
+        console.log("File: " + file);
+        console.log("isUndefined: " + isFile(file));
         
     });
 
@@ -25,12 +43,19 @@ export default function ContextMenu( {file, position}: {file: string; position: 
     return (
         <div id="menu" className="absolute bg-white rounded-lg shadow-lg py-2">
             <ul id="list_menu" className=" space-y-2">
-                <li onClick={(e)=>{contextCommand(e, file)}} id={"delete"} key={"delete"} className=" px-4 py-1 hover:bg-slate-100 hovers:text-neutral-600" >Delete       </li>
-                <li onClick={(e)=>{contextCommand(e, file)}} id={"rename"} key={"rename"} className=" px-4 py-1 hover:bg-slate-100 hover:text-neutral-600" >Rename       </li>
-                <li onClick={(e)=>{contextCommand(e, file)}} id={"file"} key={"file"} className=" px-4 py-1 hover:bg-slate-100 hover:text-neutral-600" >Create file  </li>
-                <li onClick={(e)=>{contextCommand(e, file)}} id={"folder"} key={"folder"} className=" px-4 py-1 hover:bg-slate-100 hover:text-neutral-600" >Create folder</li>
-                <li onClick={(e)=>{contextCommand(e, file)}} id={"upload"} key={"upload"} className=" px-4 py-1 hover:bg-slate-100 hover:text-neutral-600" >Upload       </li>
-                <li onClick={(e)=>{contextCommand(e, file)}} id={"download"} key={"download"} className=" px-4 py-1 hover:bg-slate-100 hover:text-neutral-600" >Download     </li>
+                <li key={"download"} > <button onClick={(e)=>{contextCommand(e, file)}} id={"download"} className=" w-full text-left px-4 py-1 hover:bg-slate-100 hover:text-neutral-600" >Download   </button> </li>
+                <li key={"delete"} > <button onClick={(e)=>{contextCommand(e, file)}} id={"delete"} className=" w-full text-left px-4 py-1 hover:bg-slate-100 hovers:text-neutral-600" >Delete          </button> </li>
+                <li key={"rename"} > <button onClick={(e)=>{contextCommand(e, file)}} id={"rename"} className=" w-full text-left px-4 py-1 hover:bg-slate-100 hover:text-neutral-600" >Rename         </button> </li>
+                {
+                    !isFile(file) ? 
+                    <>
+                        <li key={"file"} >   <button onClick={(e)=>{contextCommand(e, file)}} id={"file"} className=" w-full text-left px-4 py-1 hover:bg-slate-100 hover:text-neutral-600" >Create file        </button> </li>
+                        <li key={"folder"} > <button onClick={(e)=>{contextCommand(e, file)}} id={"folder"} className=" w-full text-left px-4 py-1 hover:bg-slate-100 hover:text-neutral-600" >Create folder  </button> </li>
+                        <li key={"upload"} > <button onClick={(e)=>{contextCommand(e, file)}} id={"upload"} className=" w-full text-left px-4 py-1 hover:bg-slate-100 hover:text-neutral-600" >Upload         </button> </li>
+                    </>:
+                    null
+                }
+                
             </ul>
         </div>
     )
